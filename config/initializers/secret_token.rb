4214b3b1ef1edd11d04c8ff098daf45d4f9b5fc7
xdite <xuite.joke@gmail.com>
2011-07-02 22:51:14 +0800

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitdown::Application.config.secret_token = '742d5952a5ac7fe17fb507075d64a327882734939946d3967873c7da52721018ec13fa6f723dafb8cd515ae78feff3741d44d6797788b14abb9500522619168c'
